7a8598ee55365482f7a707dc66a3f4285be46e65
Drew McElligott <rdmcelligott@gmail.com>
2016-11-04 11:57:14 -0500

    Makes additional corrections to userRouter

--- Server/Routes/userRouter.js
+++ Server/Routes/userRouter.js
@@ -2,18 +2,18 @@ const express = require('express')
 counst router = express.Router()
 const users = require('/Controllers/userCtrl')
 
-router.get('/', users.home)
+router.post('/', users.createUser) //add user
 
-router.get('/userId', users.userId)
+router.get('/userId', users.getUser) //get user
 
-router.put('/userId', users.userId)
+router.put('/userId', users.updateUser) //update user information
 
-router.post('/userId/saved', users.saved)
+router.post('/userId/saved', users.createPin) //add new pin
 
-router.delete('/userId/saved', users.saved)
+router.delete('/userId/saved', users.deletePin) //delete pin
 
-router.post('/userId/created', users.created)
+router.post('/userId/created', users.createSpot) //add new spot
 
-router.delete('/userId/created', users.created)
+router.delete('/userId/created', users.deleteSpot) //delete spot
 
 module.exports = router
