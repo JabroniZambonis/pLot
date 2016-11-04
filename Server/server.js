4d38b736e94fca57e54f0e2467437189aed41d2b
skbolton <stephenkbolton@gmail.com>
2016-11-04 16:07:00 -0500

    pull in env variables in server file.

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,18 @@
+// load environment variables
+require('dotenv').config({silent: true})
+
 const express = require('express')
 const app = express()
-const userRouter = require('/routes/userRouter')
+// const userRouter = require('/routes/userRouter')
 const locationRouter = require('./routes/locationRouter')
 
-app.get('/users', userRouter) //Mounts route for 'users'
+app.get('/test', function (req, res) {
+  res.send('Hey there')
+})
+
+// app.get('/users', userRouter) //Mounts route for 'users'
 app.use('/locations', locationRouter)
 
-app.listen(process.env.PORT) //Pulled from .env file
+app.listen(process.env.PORT, () => 
+  console.log(`Running at http://localhost:${process.env.PORT}`)
+) //Pulled from .env file
