9ea459b27aad8a8e148a36d9d0cdb0a8585d02e9
Drew McElligott <rdmcelligott@gmail.com>
2016-11-04 16:26:06 -0500

    Makes correction to userRouter

--- Server/Routes/userRouter.js
+++ Server/Routes/userRouter.js
@@ -2,18 +2,25 @@ const express = require('express')
 counst router = express.Router()
 const users = require('/Controllers/userCtrl')
 
-router.post('/', users.createUser) //add user
+router.route('/')
+  .post(users.createUser) //add user
 
-router.get('/userId', users.getUser) //get user
+router.route('/userId')
+  .get(users.getUser) //get user
 
-router.put('/userId', users.updateUser) //update user information
+router.route('/userId')
+  .put(users.updateUser) //update user information
 
-router.post('/userId/saved', users.createPin) //add new pin
+router.route('/userId/saved')
+  .post(users.createPin) //add new pin
 
-router.delete('/userId/saved', users.deletePin) //delete pin
+router.route('/userId/saved')
+  .delete(users.deletePin) //delete pin
 
-router.post('/userId/created', users.createSpot) //add new spot
+router.route('/userId/created')
+  .post(users.createSpot) //add new spot
 
-router.delete('/userId/created', users.deleteSpot) //delete spot
+router.route('/userId/created') 
+  .delete(users.deleteSpot) //delete spot
 
 module.exports = router
