df62fb749821f6a714dc4020d35dbdb4f5db5d4b
Drew McElligott <rdmcelligott@gmail.com>
2016-11-04 11:59:22 -0500

    Adds controller function defs and models file

--- Server/Controllers/usersCtrl.js
+++ Server/Controllers/usersCtrl.js
@@ -1,16 +1,28 @@
-var home = function(req, res) {
-  
-}
-
-var userId = function(req, res) {
+var createUser = function(req, res) {
 
 }
 
-var saved = function(req, res) {
+var getUser = function(req, res) {
 
 }
 
-var created = function(req, res) {
+var updateUser = function(req, res) {
+
+}
+
+var createPin = function(req, res) {
+
+}
+
+var deletePin = function(req, res) {
+
+}
+
+var createSpot = function(req, res) {
+
+}
+
+var deleteSpot = function(req, res) {
 
 }
 
