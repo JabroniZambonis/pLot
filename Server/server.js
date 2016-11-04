5a2f23b4a3a47bce62283e5c1f5c43ccec023d77
skbolton <stephenkbolton@gmail.com>
2016-11-04 12:03:20 -0500

    Add skeleton of basic location routing and controllers.

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const userRouter = require('/routes/userRouter')
+const locationRouter = require('./routes/locationRouter')
 
 app.get('/users', userRouter) //Mounts route for 'users'
+app.use('/locations', locationRouter)
 
 app.listen(process.env.PORT) //Pulled from .env file
