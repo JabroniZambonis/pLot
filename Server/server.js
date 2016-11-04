11e88174052f3d1ba5b1f73a960c45b0d325e39c
Drew McElligott <rdmcelligott@gmail.com>
2016-11-04 16:23:18 -0500

    Deletes some unecessary files

--- Server/server.js
+++ Server/server.js
@@ -3,16 +3,16 @@ require('dotenv').config({silent: true})
 
 const express = require('express')
 const app = express()
-// const userRouter = require('/routes/userRouter')
+const userRouter = require('/routes/userRouter')
 const locationRouter = require('./routes/locationRouter')
 
 app.get('/test', function (req, res) {
   res.send('Hey there')
 })
 
-// app.get('/users', userRouter) //Mounts route for 'users'
+app.use('/users', userRouter) //Mounts route for 'users'
 app.use('/locations', locationRouter)
 
-app.listen(process.env.PORT, () => 
+app.listen(process.env.PORT, () =>
   console.log(`Running at http://localhost:${process.env.PORT}`)
 ) //Pulled from .env file
