d1d43347e0d90dd1631c3d9e0ba022fbd7678254
Phil <plmok61@gmail.com>
2016-11-04 17:34:50 -0500

    tries and fails and adding airbnb maps

--- Client/Components/HomeMap.js
+++ Client/Components/HomeMap.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react'
 import { View, StyleSheet, MapView } from 'react-native'
 
 export default class HomeMap extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentLocation: {
+        latitude: 30.26,
+        longitude: -97.74,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1
+      }
+    }
+  }
+
+
   render () {
     return (
       <View>
         <MapView
+          region={this.state.currentLocation}
           style={{height: 200, width: 200}}
           showsUserLocation={true}
         />
