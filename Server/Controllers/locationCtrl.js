f6f3a27d508419911792421f13653a73316ba0c3
skbolton <stephenkbolton@gmail.com>
2016-11-05 11:58:21 -0500

    add search by address to location controller.

--- Server/Controllers/locationCtrl.js
+++ Server/Controllers/locationCtrl.js
@@ -1,3 +1,6 @@
+const request = require('request-promise')
+const baseGoogleURL = 
+  `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_API_KEY}`
 exports.create = function (req, res) {
 
 }
@@ -7,7 +10,13 @@ exports.findByCoords = function (req, res) {
 }
 
 exports.findByAddr = function (req, res) {
-
+  const { address } = req.query
+  const query = baseGoogleURL + `&address=${address}`
+  console.log(query)
+  request(query)
+    .then(function(response) {
+      res.json(response.location)
+    })
 }
 
 exports.photos = function (req, res) {
