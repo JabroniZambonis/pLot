d4b87587febeb15f2e646d7e722a160f359b5141
Phil <plmok61@gmail.com>
2016-11-07 11:03:34 -0600

    adds a top level App component

--- index.ios.js
+++ index.ios.js
@@ -5,43 +5,15 @@
  */
 
 import React, { Component } from 'react'
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native'
-import HomeMap from './Client/Components/HomeMap'
-import LoginPage from './Client/Components/LoginPage'
+import { AppRegistry } from 'react-native'
+import App from './Client/Components/App'
 
 export default class pLot extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <HomeMap />
-        <LoginPage/>
-      </View>
+      <App />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('pLot', () => pLot)
