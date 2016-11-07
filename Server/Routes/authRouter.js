b654b5448a3d6c47549b731f941c3b7f18b2996c
skbolton <stephenkbolton@gmail.com>
2016-11-07 17:04:09 -0600

    Basic auth route done

--- Server/Routes/authRouter.js
+++ Server/Routes/authRouter.js
@@ -1,13 +1,37 @@
 const router = require('express').Router()
 const request = require('request-promise')
-const fbUrl = "https://graph.facebook.com/v2.8/me"
+const User = require('../Models/usersModel')
+const fb = "https://graph.facebook.com/v2.8/me?fields=id,name,email,picture"
 
 router.route('/')
   .post(function(req, res) {
-    const fbAccessToken = "EAAWBgvpJAdEBAGX9g0Y2gxY6hTtny64Q3i0bpYQiZAOLqZCgvzUGOZCK9UyhBU78MUaqgs9ZBAqafe17UBRR61yqKuvfO5kjVjRsJd7h99O2CS8U0PlLOOnOfSi7vnTZCp1GqjGnk9r1w6fjXOrsE3Rpt4v7P0cOb9d8dTZC5KAZCyE2DpalQQxB1fHbEBy6sDPXNPq4Bld6ckzxBdZBw0FVs6ZCwg6HvRfwZD"
     // validate the access token with facebook
-    request(requestOptions)
-      .then(console.log)
+    request(fb + '&access_token=' + req.body.fbAccessToken)
+      .then(function (response) {
+        // on response turn into json
+        const json = JSON.parse(response)
+        new User({
+          name: json.name,
+          email: json.email,
+          fbId: json.id,
+          photo: json.picture.data.url
+        }).save()
+        .then(function (user) {
+          // user was successfully saved
+          // respond with jwt access_token
+          res.send(user)
+        })
+        .catch(function (err) {
+          // if error code 11000 then that means duplicate key
+          // in that case just update the users access token
+          if (err.code === 11000) {
+            // update the users access token
+            res.send('something')
+          } else {
+            res.status(500).json(err)
+          }
+        })
+      }) 
   })
 
 module.exports = router
