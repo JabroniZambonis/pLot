f771660a428116a7fd57e412b594ed9deda0d35a
Robert McElligott <robertmcelligott@Roberts-MacBook-Pro.local>
2016-11-07 11:29:28 -0600

    Adds basic structure of updateUser

--- Server/Controllers/usersCtrl.js
+++ Server/Controllers/usersCtrl.js
@@ -3,30 +3,36 @@ var mongoose = require('mongoose');
 var User = require('../Models/usersModel')
 
 var createUser = function(req, res) {
-  new User({name: req.body.name, photo: req.body.photo, email: req.body.email, fbId: req.body.fbId, googleId: req.body.googleId})
-    .save()
-    .then(function(data) {
-      console.log("createUser add User successful: ", data)
-    })
-    .catch(function(err) {
-      console.log("ERROR createUser add User: ", err)
-    })
+  new User({name: req.body.name,
+    photo: req.body.photo,
+    email: req.body.email,
+    fbId: req.body.fbId,
+    googleId: req.body.googleId})
+      .save()
+      .then(function(data) {
+        console.log("createUser add User successful: ", data)
+      })
+      .catch(function(err) {
+        console.log("ERROR createUser add User: ", err)
+      })
 }
 
 var getUser = function(req, res) {
-  //Take in name
   return User.find({ name: req.body.name }) //Can take a callback as a second argument if needed
   //Return what? May need to utilize callback to return data in specific format
 }
 
 var updateUser = function(req, res) {
-  //Find user_id by name
+  //What should they be able to update
 
-  User.findByIdAndUpdate(id, { $set: { name: req.body.name }}, { new: true }, function (err, name) {
-    if (err) {
-      console.log(err)
-    }
-    res.send(name);
+  User.update({ name: req.body.name},
+    { email: req.body.email },
+    { photo: req.body.photo },
+    function (err, raw) {
+      if (err) {
+        console.log(err)
+      }
+      res.send('ERROR updateUser: ', raw);
   });
 }
 
