8f2b4489dd212cb19326f33931e35dbf649cc807
Robert McElligott <robertmcelligott@Roberts-MacBook-Pro.local>
2016-11-07 19:19:03 -0600

    Makes progress on userCtrl

--- Server/Controllers/userCtrl.js
+++ Server/Controllers/userCtrl.js
@@ -10,7 +10,7 @@ exports.createUser = function(req, res) {
     googleId: req.body.googleId})
       .save()
       .then(function(data) {
-        res.status(200).send(data)
+        res.status(201).send(data)
       })
       .catch(function(err) {
         console.log("ERROR, USER NOT CREATED: ", err)
@@ -18,8 +18,8 @@ exports.createUser = function(req, res) {
 }
 
 exports.getUser = function(req, res) {
-  User.find({_id: req.params.userId}, function(err, docs) {
-    if (!err) res.status(200).send(docs)
+  User.find({_id: req.params.userId}, function(err, result) {
+    if (!err) res.status(201).send(result)
     else console.log("ERROR getUser: ", err)
   })
 }
@@ -27,41 +27,57 @@ exports.getUser = function(req, res) {
 exports.updateUser = function(req, res) {
   //What should they be able to update?
 
-  User.update({ name: req.body.name},
-    { email: req.body.email },
-    { photo: req.body.photo },
-    function (err, raw) {
-      if (err) {
-        console.log(err)
-      }
-      res.send('ERROR updateUser: ', raw);
-  });
+  // User.update({ name: req.body.name},
+  //   { email: req.body.email },
+  //   { photo: req.body.photo },
+  //   function (err, raw) {
+  //     if (err) {
+  //       console.log(err)
+  //     }
+  //     res.send('ERROR updateUser: ', raw);
+  // });
 }
 
 exports.createPin = function(req, res) {
-  let pinArray = []
-  //Query user to pull pin array, push into array variable
-  // User.find({ name: req.body.name}, function())
+  let pinArray = [] //setPin variable
 
-  //push req.body.pin into array variable
+  User.find({_id: req.body.userId}, function(err, result) { //Find user
+    if (!err) pinArray = result.createdPins //set pinArray to existing
+    else console.log("ERROR getUser: ", err)
+  })
 
-  //Update user field with pin array
+  pinArray.push(req.body.createdPin) //push in newly created Array
+
+  findOneAndUpdate({ _id: req.body.userID }, {createdPins: pinArray}) //set pins attribute to pinArray
+  //There should be a good way to refactor this rather than using
 }
 
 exports.deletePin = function(req, res) {
-  //Query user to pull pin array, find index of req.body.pin
+  let pinArray = [] //setPin variable
 
-  //splice req.body.pin out of pin array with index
+  User.find({_id: req.body.userId}, function(err, result) { //Find user
+    if (!err) pinArray = result.createdPins //set pinArray to existing
+    else console.log("ERROR getUser: ", err)
+  })
 
-  //Update user field with pin array
+  pinArray.splice()
+
+  findOneAndUpdate({ _id: req.body.userID }, {createdPins: pinArray}) //set pins attribute to pinArray
+  //There should be a good way to refactor this rather than using
 }
 
 exports.createSpot = function(req, res) {
-  //Query user to pull spot array, push into array variable
+  let spotArray = [] //setPin variable
 
-  //push req.body.spot into array variable
+  User.find({_id: req.body.userId}, function(err, result) { //Find user
+    if (!err) spotArray = result.createdSpots //set pinArray to existing
+    else console.log("ERROR getUser: ", err)
+  })
 
-  //Update user field with spot array
+  spotArray.push(req.body.createdSpots) //push in newly created Array
+
+  findOneAndUpdate({ _id: req.body.userID }, {createdSpots: SpotArray}) //set pins attribute to pinArray
+  //There should be a good way to refactor this rather than using
 }
 
 exports.deleteSpot = function(req, res) {
---
+++ Server/Lib/auth.js
@@ -0,0 +1,1 @@
+const jwt = require('/jwt.js')
