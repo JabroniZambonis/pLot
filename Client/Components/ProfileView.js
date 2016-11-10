b7b652198426d21e127ede98b1aeb4822c2c0457
ArtyEmsee <rdmcelligott@gmail.com>
2016-11-09 19:41:46 -0600

    Makes progress on profileView

--- Client/Components/ProfileView.js
+++ Client/Components/ProfileView.js
@@ -1,23 +1,46 @@
 const styles = require('../Style/style.js')
 
 import React, { Component } from 'react'
-import { View, Image } from 'react-native'
+import { Image, TouchableHighlight, Text, StyleSheet, View, Modal, TextInput } from 'react-native'
 
 export default class HomeMap extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      modalVisible: false,
     }
+
+    this.setModalVisible = this.setModalVisible.bind(this)
+  }
+
+  setModalVisible (visible) {
+    this.setState({modalVisible: visible})
   }
 
   render () {
     console.log("ProfileView.js this.props: ", this.props.currentUser.photo)
     return (
       <View>
-        <Image
-          style={styles.profileImageButton}
-          source={{uri: this.props.currentUser.photo}}
-        />
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalVisible(!this.state.modalVisible);
+          }}
+        >
+          <Image
+            style={styles.profileImageButton}
+            source={{uri: this.props.currentUser.photo}}
+          />
+        </TouchableHighlight>
+
+        <Modal
+          animationType={"slide"}
+          transparent={true}
+          visible={this.state.modalVisible}
+        >
+          <View style={{marginLeft: 22, backgroundColor: 'white'}}>
+            <Text>TEST</Text>
+          </View>
+        </Modal>
       </View>
     )
   }
