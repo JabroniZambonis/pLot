9116e64e03d1b89103e02e0c18e67363775a50f9
ArtyEmsee <rdmcelligott@gmail.com>
2016-11-10 15:09:00 -0600

    Makes progress on styling profileView

--- Server/Routes/userRouter.js
+++ Server/Routes/userRouter.js
@@ -27,20 +27,14 @@ router.route('/')
 
 router.route('/:userId')
   .get(users.getUser) //get user
-
-router.route('/:userId')
   .put(users.updateUser) //update user information
 
 router.route('/:userId/saved')
   .post(users.createPin) //add new pin
-
-router.route('/:userId/saved')
   .delete(users.deletePin) //delete pin
 
 router.route('/:userId/created')
   .post(users.createSpot) //add new spot
-
-router.route('/:userId/created')
   .delete(users.deleteSpot) //delete spot
 
 module.exports = router
