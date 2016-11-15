757218c5325502adde39af2e5a3bb22144f94a29
ArtyEmsee <rdmcelligott@gmail.com>
2016-11-15 14:49:50 -0600

    Rearranged some files for HomeMap function library

--- Client/Lib/HomeMapHelperFns.js
+++ Client/Lib/HomeMapHelperFns.js
@@ -1,3 +1,86 @@
+
+
+exports.getPinsForCoords = function(long, lat) {
+  fetch(`http://localhost:3000/locations/bycoords?long=${long}&lat=${lat}`)
+    .then(response => response.json())
+    .then(locations => {
+      const nearby = locations.map(location => {
+        return {
+          title: location.address,
+          description: location.description,
+          coordinate: {
+            longitude: location.loc[0],
+            latitude: location.loc[1]
+          },
+          rating: location.rating,
+          id: location._id
+        }
+      })
+  // set components nearby pins
+    this.setState({ nearbyLocations: nearby})
+    })
+  .catch(console.log)
+}
+
+
+exports.getAddressByCoords = function(lat, long) {
+  fetch(`http://localhost:3000/locations/googlebycoords?lat=${lat}&long=${long}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Got data: ', data)
+      this.setState({address: data})
+    })
+    .catch((err) => {
+      console.log('This did not work: ', err)
+  })
+}
+
+
+exports.setUserLocation = function() {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      let currentLocation = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }
+      this.setState({
+        currentLocation: currentLocation
+      })
+      // get pins near users location
+      this.getPinsForCoords(currentLocation.longitude, currentLocation.latitude)
+
+      //TO DO: Call function with appropriate things
+    },
+    // error finding users location
+    (error) => console.log(error),
+    // location finding settings
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+  )
+}
+
+
+exports.getUserInfo = function() {
+  fetch('http://localhost:3000/auth/login', {
+    method: 'GET',
+    headers: {
+       'Accept': 'application/json',
+       'Content-Type': 'application/json',
+       'Authorization': this.props.userToken
+    }
+  })
+  .then(response => response.json())
+  .then( (userData) => {
+    this.setState({currentUser: userData})
+  })
+  // catch any errors
+  .catch( (err) => {
+    console.log(err)
+  })
+}
+
+
 exports.searchLocationSubmit = function(event) {
   let searchString = event.nativeEvent.text
   fetch(`http://localhost:3000/locations/byaddr?q=${searchString}`)
