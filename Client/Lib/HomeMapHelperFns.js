b30aaa140d0b85e85aa409f579867a027815e819
Phil <plmok61@gmail.com>
2016-11-16 18:57:25 -0600

    fixes getPinsForCoords function

--- Client/Lib/HomeMapHelperFns.js
+++ Client/Lib/HomeMapHelperFns.js
@@ -1,15 +1,3 @@
-
-
-exports.getPinsForCoords (long, lat) {
-    fetch(`http://localhost:3000/locations/bycoords?long=${long}&lat=${lat}`)
-      .then(response => response.json())
-      .then(locations => {
-        console.log('locations: ',locations)
-        this.setState({ nearbyLocations: locations})
-      })
-      .catch(console.log)
-  }
-
 exports.getAddressByCoords = function(lat, long) {
   fetch(`http://localhost:3000/locations/googlebycoords?lat=${lat}&long=${long}`)
     .then(response => response.json())
@@ -164,24 +152,9 @@ exports.getPinsForCoords = function(long, lat) {
   fetch(`http://localhost:3000/locations/bycoords?long=${long}&lat=${lat}`)
     .then(response => response.json())
     .then(locations => {
-      let nearby = locations.map(location => {
-        return {
-          title: location.address,
-          description: location.description,
-          coordinate: {
-            longitude: location.loc[0],
-            latitude: location.loc[1]
-          },
-          rating: location.rating,
-          id: location._id
-        }
-      })
-      // set components nearby pins
-      this.setState({ nearbyLocations: nearby})
+      this.setState({ nearbyLocations: locations})
     })
-    .catch((err) => {
-      console.log("ERROR get getPinsForCoords in HomeMapHelperFns.js: ", err)
-    })
+    .catch(console.log)
 }
 
 
