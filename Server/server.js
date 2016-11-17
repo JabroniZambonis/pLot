168ee31e72b67495bba01d8f8d145415779c146f
skbolton <stephenkbolton@gmail.com>
2016-11-17 13:55:44 -0600

    force pushing heroku

--- Server/server.js
+++ Server/server.js
@@ -3,9 +3,9 @@ require('dotenv').config({silent: true})
 
 const express = require('express')
 const app = express()
-// const userRouter = require('./Routes/userRouter')
-// const locationRouter = require('./Routes/locationRouter')
-// const authRouter = require('./Routes/authRouter')
+const userRouter = require('./Routes/userRouter')
+const locationRouter = require('./Routes/locationRouter')
+const authRouter = require('./Routes/authRouter')
 const bodyParser = require('body-parser')
 
 //app level middleware
@@ -17,9 +17,9 @@ app.get('/test', function (req, res) {
   res.send('Hey there')
 })
 
-// app.use('/users', userRouter) //Mounts route for 'users'
-// app.use('/locations', locationRouter)
-// app.use('/auth', authRouter)
+app.use('/users', userRouter) //Mounts route for 'users'
+app.use('/locations', locationRouter)
+app.use('/auth', authRouter)
 
 app.listen(process.env.PORT, () =>
   console.log(`Running at http://localhost:${process.env.PORT}`)
