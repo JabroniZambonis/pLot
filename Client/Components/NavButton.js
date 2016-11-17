2a235f12d366a0651a9cf6ce02ac54c47aabc819
Zach <zachraymer@gmail.com>
2016-11-17 16:25:25 -0600

    adds back to map button

--- Client/Components/NavButton.js
+++ Client/Components/NavButton.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { TouchableHighlight, Text, StyleSheet } from 'react-native'
+import { TouchableHighlight, Text, StyleSheet, Image } from 'react-native'
 import Button from 'apsl-react-native-button'
 
 const styles = require('../Style/style.js')
 
 export default ({ onPress, text }) => (
-  <Button onPress={onPress} style={styles.navButtonBack} activeOpacity={.08}>
+  <Button onPress={onPress} style={styles.navButtonBack}>
+  	<Image source={require('../Public/back-Arrow.png')} style={styles.backArrow}/>
   	<Text style={styles.navButtonBackText}>{text}</Text>
   </Button>
 )
