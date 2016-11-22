081147587217852184bc1d1667b821d73585be65
Zach <zachraymer@gmail.com>
2016-11-21 23:31:26 -0600

    style tweaks

--- Client/Components/ProfileView.js
+++ Client/Components/ProfileView.js
@@ -1,7 +1,7 @@
 const styles = require('../Style/style.js')
 
 import React, { Component } from 'react'
-import { Image, TouchableHighlight, Text,View} from 'react-native'
+import { Image, TouchableOpacity, Text,View} from 'react-native'
 
 export default class ProfileView extends Component {
   constructor(props) {
@@ -14,11 +14,20 @@ export default class ProfileView extends Component {
 
   render () {
     return (
-      <View style={{flex:1,backgroundColor:'white',}}>
-        <TouchableHighlight style={styles.profileViewButton} onPress={this.finishedLogout.bind(this)}>
-          <Text style={styles.profileViewText}>Log Out</Text>
-        </TouchableHighlight>
-      </View>
+        <View style={{flex:1,backgroundColor:'white',}}>
+          <TouchableOpacity style={styles.profileViewButton}>
+            <Text style={styles.profileViewText}>Created Pins</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.profileViewButton}>
+            <Text style={styles.profileViewText}>Help</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.profileViewButton}>
+            <Text style={styles.profileViewText}>About</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.profileViewButton} onPress={this.finishedLogout.bind(this)}>
+            <Text style={styles.profileViewText}>Log Out</Text>
+          </TouchableOpacity>
+        </View>
     )
   }
 }
